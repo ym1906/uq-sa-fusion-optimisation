8b3d23b78f7b6bbb5e703c6008abf0b374a8efac
ym1906 <graeme.turkington@ukaea.uk>
2024-05-16 16:04:32 +0100

    clean up, begin to fix the tests for new functions

--- tests/test_uncertainty_data.py
+++ tests/test_uncertainty_data.py
@@ -23,47 +23,20 @@ class TestUncertaintyData(unittest.TestCase):
         # Test if merging HDF files returns a DataFrame
         uq_data = UncertaintyData(
             path_to_uq_data_folder="/home/graeme/easyVVUQ-process/demo_runs_2/run1/",
-            figure_of_merit="sqsumsq",
-            input_parameters=["aspect", "tbrnmn"],
+            sampled_variables=["aspect", "tbrnmn"],
         )
         merged_df = uq_data.merge_hdf_files()
         self.assertIsInstance(merged_df, pd.DataFrame)
 
-    def test_calculate_sensitivity(self):
+    def test_calculate_sensitivity(self, sampled_variables):
         # Test if sensitivity calculation returns a DataFrame
         uq_data = UncertaintyData(
             path_to_uq_data_folder="/home/graeme/easyVVUQ-process/demo_runs_2/run1/",
-            figure_of_merit="sqsumsq",
-            input_parameters=["aspect", "tbrnmn"],
+            sampled_variables=["aspect", "tbrnmn"],
         )
         uq_data.uncertainties_df = self.test_data
         uq_data.calculate_sensitivity(figure_of_merit="rmajor")
         self.assertIsInstance(uq_data.sensitivity_df, pd.DataFrame)
-
-    # def test_find_significant_parameters(self):
-    #     # Test if significant parameters are found
-    #     uq_data = UncertaintyData(
-    #         path_to_uq_data_folder="/home/graeme/easyVVUQ-process/demo_runs_2/run1/",
-    #         figure_of_merit="sqsumsq",
-    #         input_parameters=["aspect", "tbrnmn"],
-    #     )
-    #     uq_data.sensitivity_df = pd.DataFrame(
-    #         {"S1": [0.1, 0.05]}, index=["aspect", "tbrnmn"]
-    #     )
-    #     significant_params = uq_data.find_significant_parameters(
-    #         uq_data.sensitivity_df, "S1", significance_level=0.05
-    #     )
-    #     self.assertEqual(significant_params, ["aspect"])
-
-    # def test_estimate_design_values(self):
-    #     # Test if design values are estimated correctly
-    #     uq_data = UncertaintyData(
-    #         path_to_uq_data_folder="path/to/folder",
-    #         figure_of_merit="sqsumsq",
-    #         input_parameters=["param1", "param2"],
-    #     )
-    #     design_values = uq_data.estimate_design_values(["param1", "param2"])
-    #     self.assertIsInstance(design_values, pd.Series)
 
 
 if __name__ == "__main__":
--- tests/test_uncertainty_optimisation.py
+++ tests/test_uncertainty_optimisation.py
@@ -3,18 +3,17 @@ import pandas as pd
 import sys
 
 sys.path.append("/home/graeme/process_uq/uq_tools")
-from confidence_analysis import ConfidenceAnalysis
+from uncertainty_optimisation import UncertaintyOptimisation
 from uncertainty_data import UncertaintyData
 
 
-class TestConfidenceAnalysis(unittest.TestCase):
+class TestUncertaintyOptimisation(unittest.TestCase):
 
     def setUp(self):
         # Set up any data or configurations needed for tests
         self.uq_data = UncertaintyData(
             path_to_uq_data_folder="/home/graeme/easyVVUQ-process/demo_runs_2/run1/",
-            figure_of_merit="sqsumsq",
-            input_parameters=["aspect", "tbrnmn"],
+            sampled_variables=["aspect", "tbrnmn"],
         )
         self.input_names = ["aspect", "tbrnmn"]
         self.number_intervals = 2
@@ -22,25 +21,29 @@ class TestConfidenceAnalysis(unittest.TestCase):
         self.weight_overlap = 0.5
 
     def test_initialization(self):
-        # Test initialization of ConfidenceAnalysis instance
+        # Test initialization of UncertaintyOptimisation instance
 
-        confidence_analysis = ConfidenceAnalysis(
+        uncertainty_optimisation = UncertaintyOptimisation(
             uq_data=self.uq_data,
             input_names=self.input_names,
             number_intervals=self.number_intervals,
             weight_confidence=self.weight_confidence,
             weight_overlap=self.weight_overlap,
         )
-        self.assertIsInstance(confidence_analysis, ConfidenceAnalysis)
-        self.assertEqual(confidence_analysis.number_intervals, self.number_intervals)
-        self.assertEqual(confidence_analysis.weight_confidence, self.weight_confidence)
-        self.assertEqual(confidence_analysis.weight_overlap, self.weight_overlap)
-        self.assertEqual(confidence_analysis.uq_data, self.uq_data)
-        self.assertEqual(confidence_analysis.input_names, self.input_names)
+        self.assertIsInstance(uncertainty_optimisation, UncertaintyOptimisation)
+        self.assertEqual(
+            uncertainty_optimisation.number_intervals, self.number_intervals
+        )
+        self.assertEqual(
+            uncertainty_optimisation.weight_confidence, self.weight_confidence
+        )
+        self.assertEqual(uncertainty_optimisation.weight_overlap, self.weight_overlap)
+        self.assertEqual(uncertainty_optimisation.uq_data, self.uq_data)
+        self.assertEqual(uncertainty_optimisation.input_names, self.input_names)
 
     # def test_calculate_metric(self):
     #     # Test the calculate_metric method
-    #     confidence_analysis = ConfidenceAnalysis(
+    #     uncertainty_optimisation = UncertaintyOptimisation(
     #         uq_data=self.uq_data,
     #         input_names=self.input_names,
     #         number_intervals=self.number_intervals,
@@ -49,10 +52,10 @@ class TestConfidenceAnalysis(unittest.TestCase):
     #     )
     #     confidences = [0.8, 0.7, 0.9]
     #     errors = [0.1, 0.05, 0.15]
-    #     expected_metric = 1.0 * sum(confidences) - 0.5 * confidence_analysis.overlap(
+    #     expected_metric = 1.0 * sum(confidences) - 0.5 * uncertainty_optimisation.overlap(
     #         confidences, errors
     #     )
-    #     calculated_metric = confidence_analysis.calculate_metric(
+    #     calculated_metric = uncertainty_optimisation.calculate_metric(
     #         confidences, errors, self.weight_confidence, self.weight_overlap
     #     )
     #     self.assertAlmostEqual(calculated_metric, expected_metric, places=5)
